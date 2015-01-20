99823eae74e2ad27c787d6070288e62ea8bbf08f
Chris Rempola <chrisxstyles@gmail.com>
2015-01-20 14:43:34 -0800

    first pass working, able to add/delete items via the buttons

--- shopping-list/js/app.js
+++ shopping-list/js/app.js
@@ -1,10 +1,45 @@
 $(document).ready(function(){
-	// on enter key, print value
+	// Input item and add when Enter is pressed
 	$(document).keyup(function(e) {
   		if(e.keyCode==13) {
   			enteredItem = $('#add-items').val();
-  			$("#list").append("<li>" + enteredItem + "</li>");
+  			$("#list").append('<li class="active-item">' + '<i class="fa fa-square-o"></i>\n' + enteredItem + "</li>");
 	  	}
+	  	addItem();
+
 	});
+
+	// $("li > i").on("click", function(){
+	// 	//alert("clicked");
+	// 	if($(this).hasClass("fa-square-o")) {
+	// 		$(this).parent().removeClass("active-item").addClass("completed-item");
+	// 		$(this).removeClass("fa-square-o").addClass("fa-check-square-o");
+	// 	} 
+	// 	else {
+	// 		$(this).parent().removeClass("completed-item").addClass("active-item");
+	// 		$(this).removeClass("fa-check-square-o").addClass("fa-square-o");
+	// 	}
+	// });
+
+	addItem();
+
+    function addItem(){
+    	$("li > i").on("click", function(){
+			//alert("clicked");
+			if($(this).hasClass("fa-square-o")) {
+				$(this).parent().removeClass("active-item").addClass("completed-item");
+				$(this).removeClass("fa-square-o").addClass("fa-check-square-o");
+			} 
+			else {
+				$(this).parent().removeClass("completed-item").addClass("active-item");
+				$(this).removeClass("fa-check-square-o").addClass("fa-square-o");
+			}
+		});
+    };
+
+    $("#delete-items").on("click", function(){
+    	//alert("clicked");
+  		$('.completed-item').remove();
+    });
 
 });
