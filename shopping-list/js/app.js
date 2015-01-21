f7b9dd1659ba36e5d3cdd421509c9187c6b62a6b
Chris Rempola <chrisxstyles@gmail.com>
2015-01-20 21:17:38 -0800

    fixes from Neville's suggestions

--- shopping-list/js/app.js
+++ shopping-list/js/app.js
@@ -5,37 +5,20 @@ $(document).ready(function(){
   			enteredItem = $('#add-items').val();
   			$("#list").append('<li class="active-item">' + '<i class="fa fa-square-o"></i>\n' + enteredItem + "</li>");
 	  	}
-	  	addItem();
 
 	});
 
-	// $("li > i").on("click", function(){
-	// 	//alert("clicked");
-	// 	if($(this).hasClass("fa-square-o")) {
-	// 		$(this).parent().removeClass("active-item").addClass("completed-item");
-	// 		$(this).removeClass("fa-square-o").addClass("fa-check-square-o");
-	// 	} 
-	// 	else {
-	// 		$(this).parent().removeClass("completed-item").addClass("active-item");
-	// 		$(this).removeClass("fa-check-square-o").addClass("fa-square-o");
-	// 	}
-	// });
-
-	addItem();
-
-    function addItem(){
-    	$("li > i").on("click", function(){
-			//alert("clicked");
-			if($(this).hasClass("fa-square-o")) {
-				$(this).parent().removeClass("active-item").addClass("completed-item");
-				$(this).removeClass("fa-square-o").addClass("fa-check-square-o");
-			} 
-			else {
-				$(this).parent().removeClass("completed-item").addClass("active-item");
-				$(this).removeClass("fa-check-square-o").addClass("fa-square-o");
-			}
-		});
-    };
+    $("#list").on("click", "i", function(){
+		//alert("clicked");
+		if($(this).hasClass("fa-square-o")) {
+			$(this).parent().removeClass("active-item").addClass("completed-item");
+			$(this).removeClass("fa-square-o").addClass("fa-check-square-o");
+		} 
+		else {
+			$(this).parent().removeClass("completed-item").addClass("active-item");
+			$(this).removeClass("fa-check-square-o").addClass("fa-square-o");
+		}
+	});
 
     $("#delete-items").on("click", function(){
     	//alert("clicked");
